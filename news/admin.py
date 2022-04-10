96f5a7d66571304e543dceaf6ed4771527b925c6
Kosteykin <kostyaann1@gmail.com>
2022-04-10 13:50:36 +0300

    list display admin

--- news/admin.py
+++ news/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
+
 from news.models import Post
 
-admin.site.register(Post)
+
+class NewsPost(admin.ModelAdmin):
+    list_display = ('post_title', 'post_publication_date', 'post_category', 'post_author')
+
+
+admin.site.register(Post, NewsPost)
